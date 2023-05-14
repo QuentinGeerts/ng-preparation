cbb03bc098616b025ca0c42220955cf4c2a47b6f
QuentinGeerts <quentin.geerts@bstorm.be>
2023-05-14 14:13:21 +0200

    Demo 10 - Ajout du HostBinding

--- src/app/demos/demo10/highlight.directive.ts
+++ src/app/demos/demo10/highlight.directive.ts
@@ -1,9 +1,9 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnInit {
 
   constructor (private el: ElementRef) { }
 
@@ -24,6 +24,23 @@ export class HighlightDirective {
   onMouseClick () {
     this.el.nativeElement.style.backgroundColor = this.defaultColor;
     this.el.nativeElement.style.color = "red";
+  }
+
+  @Input()
+  @HostBinding('class') // permet de définir la valeur d’une propriété sur les hôtes de la directive
+  private class!: string;
+
+  @HostBinding('attr.role')
+  private role!: string;
+
+  @HostBinding('style.backgroundColor')
+  private color!: string;
+
+  ngOnInit (): void {
+    console.log(this.class);
+    this.class = 'test'; // Ajoute la classe test à l'attribut class qui se trouve sur l'élément <!> ne fonctionne qu'avec class
+    this.role = 'admin';
+    this.color = 'yellow';
   }
 
 }
