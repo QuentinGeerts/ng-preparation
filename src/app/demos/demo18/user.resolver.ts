21384dd5373240c64b21c4791c87977b049d460d
QuentinGeerts <quentin.geerts@bstorm.be>
2023-05-14 14:41:18 +0200

    upgrade project to v16.0.1

--- src/app/demos/demo18/user.resolver.ts
+++ src/app/demos/demo18/user.resolver.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  Resolve,
-  RouterStateSnapshot
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserResolver implements Resolve<any> {
+export class UserResolver  {
 
   constructor (private userService: UserService) { }
 
